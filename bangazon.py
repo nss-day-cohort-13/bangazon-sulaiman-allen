caa940171d4e1f33805a5feabd8ab06ebb94c84d
Sulaiman.Allen <xalerons@gmail.com>
2016-07-28 17:51:08 -0500

    Added more logic

--- bangazon.py
+++ bangazon.py
@@ -16,7 +16,6 @@ class Bangazon():
         print("1. New User Account\n2. Select User\n3. View Chirps")
         print("4. Public Chirp\n5. Private Chirp\n6. Exit")
         print()
-
         choice = int(input("> "))
         print()
 
@@ -24,64 +23,16 @@ class Bangazon():
             self.create_user()
 
         elif (choice == 2):  # Select user
-            # This menu chooses from a list of established users to login as.
-
-            # Which user is chriping?
-            # 1. Tweedleedee
-            # 2. BiffBoffin
-            # ...
-
-            print("Which user is chriping?")
-            # show self.users here
-            self.user = input("> ")
+            self.select_user()
 
         elif (choice == 3):  # View Chirps
+            self.view_chirps()
 
-            pass
-            # Only the two users involved in a private chirp can see it in their Private Chirps section.
-            # << Private Chirps >>
-            # 1. BiffBoffin: Hey, you up for ping...
-            # 2. Lara_keet: Any idea what Jeff wa...
-            # 3. BiffBoffin: Hah, you got wrecked...
-            # << Public Chirps >>
-            # 4. Tweedleedee: Anybody know a good...
-            # 5. Fuzzy: Do NOT try the mega ultra...
-            # 6. Velton32: You guys have got to s...
-            # ...
-            # 9. Main Menu
-            # >
-
-            # Selecting an individual chirp takes you to that chirp's comment thread.
-
-            # Tweedleedee: Anybody know a good Thai restaraunt in the area?
-            # Fuzzy: Smiling Elephant is really good
-            # BiffBoffin: The pad krapow is amazing!
-            # ...
-            # 1. Reply
-            # 2. Back
-            # >
         elif (choice == 4):  # Public Chirp
-            # Public
-
-            # Enter chirp text
-            # >
-
-            # Users can chirp publicly or they can start a private chirp with another user.
-            pass
+            self.new_public_chirp()
 
         elif (choice == 5):  # Private Chirp
-            # Private
-
-            # Chirp at
-            # 1. BiffBoffin
-            # 2. Lara_keet
-            # ...
-            # 9. Cancel
-            # >
-
-            # Enter chirp text
-            # >
-            pass
+            self.new_private_chirp()
 
         elif (choice == 6):  # Exit
             print("Goodbye")
@@ -98,6 +49,67 @@ class Bangazon():
         print()
         # write user to file
         self.menu()
+
+    def select_user(self):
+
+        # This menu chooses from a list of established users to login as.
+        print("Which user is chriping?")
+        # show self.users here
+        # 1. Tweedleedee
+        # 2. BiffBoffin
+        # ...
+        self.user = input("> ")
+
+    def view_chirps(self):
+
+        # Only the two users involved in a private chirp can see it in their Private Chirps section.
+        print("<< Private Chirps >>")
+        # 1. BiffBoffin: Hey, you up for ping...
+        # 2. Lara_keet: Any idea what Jeff wa...
+        # 3. BiffBoffin: Hah, you got wrecked...
+
+        print("<< Public Chirps >>")
+        # 4. Tweedleedee: Anybody know a good...
+        # 5. Fuzzy: Do NOT try the mega ultra...
+        # 6. Velton32: You guys have got to s...
+        print("0. Main Menu")
+        print()
+        choice = int(input("> "))
+
+        # Selecting an individual chirp takes you to that chirp's comment thread.
+
+        # Tweedleedee: Anybody know a good Thai restaraunt in the area?
+        # Fuzzy: Smiling Elephant is really good
+        # BiffBoffin: The pad krapow is amazing!
+        # ...
+        # 1. Reply
+        # 2. Back
+        # >
+        pass
+
+    def new_public_chirp(self):
+
+        chirp = input("Enter chirp text\n> ")
+
+        # Users can chirp publicly or they can start a private chirp with another user.
+
+        # A chirp ID number
+        # Who authored the chirp
+        # Is this a public or private chirp
+        # Who the chirp is to (if applicable)
+        # The text content of the chirp
+
+    def new_private_chirp(self):
+        # Private
+
+        # Chirp at
+        # 1. BiffBoffin
+        # 2. Lara_keet
+        # ...
+        # 9. Cancel
+        # >
+
+        chirp = input("Enter chirp text\n> ")
 
 
 if __name__ == '__main__':
