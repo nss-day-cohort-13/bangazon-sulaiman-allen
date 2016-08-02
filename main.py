a03971914e8711273cacb45cc622ca714bf0a661
xalerons <xalerons@gmail.com>
2016-08-02 00:17:25 -0500

    Added proper error handling

--- main.py
+++ main.py
@@ -31,43 +31,40 @@ class main():
         print("4. Public Chirp\n5. Private Chirp\n6. Exit")
         print()
 
+        choice = input("> ")
+
+        return self.menu_selection(choice)
+
+    def menu_selection(self, choice):
+
         # If the user enters invalid choice, make sure that the error is caught
         # and send them back to the display_menu
         try:
-            choice = int(input("> "))
+            if (isinstance(int(choice), int) and (choice > 0 and choice <= 6)):
 
-            if (isinstance(choice, int) and (choice > 0 and choice <= 6)):
-                return self.menu_selection(choice)
+                if (choice == 1):  # New user account
+                    self.create_user()
+
+                elif (choice == 2):  # Select user
+                    self.select_user()
+
+                elif (choice == 3):  # View Chirps
+                    self.view_chirps()
+
+                elif (choice == 4):  # Public Chirp
+                    self.new_public_chirp()
+
+                elif (choice == 5):  # Private Chirp
+                    self.new_private_chirp()
+
+                elif (choice == 6):  # Exit
+                    print()
+                    print("Goodbye")
+                    exit()
             else:
                 raise ValueError()
         except ValueError:
             self.display_menu()
-
-    def menu_selection(self, choice):
-
-        if (choice == 1):  # New user account
-            self.create_user()
-
-        elif (choice == 2):  # Select user
-            self.select_user()
-
-        elif (choice == 3):  # View Chirps
-            self.view_chirps()
-
-        elif (choice == 4):  # Public Chirp
-            self.new_public_chirp()
-
-        elif (choice == 5):  # Private Chirp
-            self.new_private_chirp()
-
-        elif (choice == 6):  # Exit
-            print()
-            print("Goodbye")
-            exit()
-
-        else:
-            print("Please enter a valid choice.")
-            self.menu()
 
 
 if __name__ == '__main__':
